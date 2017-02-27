808ef2a3aedd6cce318c5a1796307cbad688c8e1
brotherjudas <brotherjudas@ARCADIA-1138>
2017-02-27 11:01:29 +0200

    test convert bitset->bit string

--- gann/src/gann/Main.java
+++ gann/src/gann/Main.java
@@ -1,10 +1,21 @@
 package gann;
 
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world!");
+		BitSet test = new BitSet(6);
+		test.set(3,6);
+		
+		for(int i=1;i<=test.length();i++)
+			if(test.get(i))
+				System.out.print(1);
+			else
+				System.out.print(0);
+		
+		System.out.println();
 
 	}
 
